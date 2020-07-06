cab0529d25e407432407826515f8dbe2c5e3629b
dan. teegan <dan.teegan@hotmail.com>
2020-07-06 09:46:00 +0100

    Polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,19 @@
+class Dog:
+
+
+    def intro(self):
+        print("There are many types of Dogs.")
+
+    def run(self):
+        print("All dogs can run.")
+
+
+class lab(Dog):
+    def run(self):
+        print("Labridors can run")
+
+
+class jack(Dog):
+    def run(self):
+        print("Cant run, injured at the moment")
+
